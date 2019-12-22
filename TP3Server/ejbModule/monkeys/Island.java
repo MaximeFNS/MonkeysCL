08f4ffe6a87fbd8896f16793c8016f1549b7ce7b
Maxime <maxime.afonso@reseau.eseo.fr>
2019-12-22 19:33:54 +0100

    Oubli Island

--- TP3Server/ejbModule/monkeys/Island.java
+++ TP3Server/ejbModule/monkeys/Island.java
@@ -1,18 +1,40 @@
 package monkeys;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 /**
  * @author Mickael Clavreul
  */
 @Entity
-public class Island {
-	
+@Table(name = "ISLANDS")
+public class Island implements Serializable {
+
+	@Id
+	@Column(name = "ISLAND_ID")
 	private int id;
-	
+
 	private int[][] map;
+	protected Collection<Element> elements = new ArrayList<Element>();
+
+	@OneToMany(mappedBy = "island", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	public Collection<Element> getElements() {
+		return elements;
+	}
+
+	public void setElements(Collection<Element> elements) {
+		this.elements = elements;
+	}
 
 	public Island() {
 		this.map = null;
